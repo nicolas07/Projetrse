38c87db54e7f8ca5ac6cab6ed1551b4286ec98af
Nicolas Kleber <nkleber@premiumconsulting.fr>
2019-02-13 11:41:00 +0100

    Implémentation IHM ReturnPackageFragment

--- app/src/main/java/com/example/projet_rse/ReturnPackageFragment.java
+++ app/src/main/java/com/example/projet_rse/ReturnPackageFragment.java
@@ -1,12 +1,24 @@
 package com.example.projet_rse;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class ReturnPackageFragment extends Fragment {
+
+    private EditText NumberPackages;
+    private TextView Address;
+    private Button ValidateReturn;
 
         public static ReturnPackageFragment newInstance() {
             return (new ReturnPackageFragment());
@@ -14,6 +26,29 @@ public class ReturnPackageFragment extends Fragment {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_returnpackage, container, false);
+
+            View view = inflater.inflate(R.layout.fragment_returnpackage, container, false);
+            NumberPackages = (EditText) view.findViewById(R.id.et_packageNumber);
+            SharedPreferences settings = this.getActivity().getSharedPreferences("PREFS", 0);
+            NumberPackages.setText(settings.getString("value", ""));
+
+            Address = (TextView) view.findViewById(R.id.tv_address);
+            ValidateReturn = (Button) view.findViewById(R.id.bt_validateReturn);
+            ValidateReturn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int n = Integer.parseInt(NumberPackages.getText().toString());
+                    String message = null;
+                    if(n > 0 && TextUtils.isEmpty(Address.getText().toString())){
+                        message = "Retour validée";
+                    } else {
+                        message = "Erreur";
+                    }
+                    Toast.makeText(getContext(),message,Toast.LENGTH_LONG).show();
+
+
+                }
+            });
+            return view;
         }
 }
