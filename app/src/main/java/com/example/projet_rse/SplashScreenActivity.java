e450d2c4b42509c0ab5584276630de2c1bf06b0b
Nicolas <laheiris@gmail.com>
2019-02-16 12:54:24 +0100

    Maj SplashScreen

--- app/src/main/java/com/example/projet_rse/SplashScreenActivity.java
+++ app/src/main/java/com/example/projet_rse/SplashScreenActivity.java
@@ -1,19 +1,27 @@
 package com.example.projet_rse;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_DUREE_OUT = 3000; //en msecondes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Intent intent = new Intent(getApplicationContext(),
-                MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_DUREE_OUT);
     }
 }
