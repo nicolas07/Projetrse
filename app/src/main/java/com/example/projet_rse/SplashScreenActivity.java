bb09845ed34db7c0775bfe4c298d76d7444cbd22
Nicolas <laheiris@gmail.com>
2019-03-12 23:07:08 +0100

    Mise à jour Splash Screen

--- app/src/main/java/com/example/projet_rse/SplashScreenActivity.java
+++ app/src/main/java/com/example/projet_rse/SplashScreenActivity.java
@@ -1,9 +1,13 @@
 package com.example.projet_rse;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -14,8 +18,16 @@ public class SplashScreenActivity extends AppCompatActivity {
         //Set Status bar color
         getWindow().setStatusBarColor(getColor(R.color.colorPrimary));
 
+        TextView textView = findViewById(R.id.tv_Label);
+
+        SpannableString spannableString = new SpannableString(getResources().getString(R.string.app_name));
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorLogo)),0,1,0);
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorLogo)),6,7,0);
+
+        textView.setText(spannableString);
+
         //en msecondes
-        int SPLASH_DUREE_OUT = 3000;
+        int SPLASH_DUREE_OUT = 5000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
